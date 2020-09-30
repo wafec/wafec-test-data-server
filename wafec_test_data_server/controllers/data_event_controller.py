386e64109de4e7156e869ea11a6dd5099e7486f7
Wallace <wallacefcardoso@gmail.com>
2020-09-30 15:35:12 -0300

    session close

--- wafec_test_data_server/controllers/data_event_controller.py
+++ wafec_test_data_server/controllers/data_event_controller.py
@@ -14,10 +14,17 @@ data_event_controller = Blueprint('data_event_controller', __name__)
 @data_event_controller.route('/api/data_event/', methods=['POST'])
 def post():
     session = Session()
-    data_event = DataEvent()
-    data_event.source = request.json['source']
-    data_event.event_name = request.json['event_name']
-    data_event.value = request.json['value']
-    session.add(data_event)
-    session.commit()
-    return jsonify({'id': data_event.id})
+    try:
+        data_event = DataEvent()
+        data_event.source = request.json['source']
+        data_event.event_name = request.json['event_name']
+        data_event.value = request.json['value']
+        session.add(data_event)
+        session.commit()
+        return jsonify({'id': data_event.id})
+    except Exception as exc:
+        session.rollback()
+        return str(exc), 500
+    finally:
+        session.close()
+
